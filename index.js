1172fe948b910714a0aef1a1772f411c0c3096d9
Austin Milbin <acmilbin@gmail.com>
2017-11-11 19:04:14 +0000

    automatically backed up by learn

--- index.js
+++
@@ -1,4 +0,0 @@
-const currentUser = "Grace Hopper";
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
-const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = `Welcome, ${currentUser.slice(0,1)!}`;
